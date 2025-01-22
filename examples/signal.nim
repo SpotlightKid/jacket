2c99d183bab5b96c3d630beef35ff83c8868a872
Christopher Arndt <chris@chrisarndt.de>
2025-01-22 23:13:01 +0100

    fix: define SIGTERM on windows, since it is not exported by system/ansi_c there
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- examples/signal.nim
+++ examples/signal.nim
@@ -1,13 +1,15 @@
 import system/ansi_c
 
-export SIG_DFL, SIGABRT, SIGFPE, SIGILL, SIGINT, SIGSEGV, SIGTERM
+export SIG_DFL, SIGABRT, SIGFPE, SIGILL, SIGINT, SIGSEGV
 
 when not defined(windows):
-  export SIGPIPE
+  export SIGPIPE, SIGTERM
   var
     SIG_IGN* {.importc: "SIG_IGN", header: "<signal.h>".}: cint
     SIGHUP* {.importc: "SIGHUP", header: "<signal.h>".}: cint
     SIGQUIT* {.importc: "SIGQUIT", header: "<signal.h>".}: cint
+else:
+  const SIGTERM* = cint(15)
 
 type CSighandlerT = proc (a: cint) {.noconv.}
 
